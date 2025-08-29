7cd5daf6583024e7026ffa48a480f7227c6fb340
segni kasahun <segnikasahun969@gmail.com>
2025-08-29 16:17:49 -0700

    home page

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import Hero from "./components/Hero";
+import Product from "./components/Product";
+import Service from "./components/Service";
+
+
+function App() {
+  return (
+    <>
+      
+      <Hero />
+      <About />
+      <Product />
+      <Service />
+      <Blog />
+      <Gallery />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

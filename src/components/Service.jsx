5e94a825be2935dbab15c204d46d25023f5ba463
segni kasahun <segnikasahun969@gmail.com>
2025-09-22 12:31:06 -0700

    service page

---
+++ src/components/Service.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <section>
+        <div>
+            <div className='w-[100px] h-[100px] bg-grey-500'>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Service

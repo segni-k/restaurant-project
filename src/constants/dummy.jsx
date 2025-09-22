516d499f5d6d7275c1e6d831a18fd1213143f221
segni kasahun <segnikasahun969@gmail.com>
2025-09-22 12:31:26 -0700

    dummy data

---
+++ src/constants/dummy.jsx
@@ -0,0 +1,110 @@
+
+import cheese from "../assets/burger-1.jpg"
+import fish from "../assets/burger-2.jpg"
+import chicken from "../assets/burger-3.jpg"
+import Royal from "../assets/burger-4.jpg"
+import beef from "../assets/burger-5.jpg"
+import double from "../assets/burger-6.jpg"
+import post1 from '../assets/Flux_Dev_Immerse_yourself_in_a_world_of_culinary_artistry_and__0.jpeg'
+import post2 from '../assets/Flux_Dev_Immerse_yourself_in_a_world_of_culinary_artistry_and__1.jpeg'
+import post3 from '../assets/Flux_Dev_Immerse_yourself_in_a_world_of_culinary_artistry_and__3.jpeg'
+
+
+export const blogData = [
+    {
+        title: "Exploring Local culinary Gems: A Foodie's Guide." ,
+        discription: " Join Emily as she takes you on a gastronomic adventure through the neighborhood surrounding our hotel.",
+        img: post1,
+        name: "Betty",
+        date: "25th March 2024"
+    },
+    {
+        title: "Balancing Mind, Body, and Soul at Our Hotel." ,
+        discription: " Discover holistic spa treatments, fitness facilities, and mindfulness practices that will leave you feeling refreshed.",
+        img: post2,
+        name: "Jibril",
+        date: "22th June 2024"
+    },
+    {
+        title: "Exploring Outdoor Activities Near Our Hotels." ,
+        discription: " From hiking and biking trails to water sports and wildlife encounters, she highlights ways to experience nature's wonders.",
+        img: post3,
+        name: "Sarah",
+        date: "10th April 2024"
+    },
+]
+
+export const dummy = [
+        
+    {
+        title: "Cheese Burger",
+        dicription: "Try our best Cheese Burger",
+        price: 280,
+        img: cheese,
+        alt: "Cheese Burger" 
+
+    },
+    {
+        title: "Fish Burger",
+        dicription: "Try our best Fish Burger",
+        price: 340,
+        img: fish,
+        alt: "Fish burger"
+
+    },
+    {
+        title: "Chicken Burger",
+        dicription: "Try our best Chicken burger",
+        price: 320,
+        img: chicken,
+        alt: "Chicken Burger"
+
+    },
+    {
+        title: "Royal Special Burger",
+        dicription: "Special burger of the house",
+        price: 450,
+        img: Royal,
+        alt: "Royal Special Burger"
+
+    },
+    {
+        title: "Beef Burger" ,
+        dicription: "Try our best Beef Burger" ,
+        price: 200,
+        img: beef,
+        alt: "Beef Burger" 
+
+    },
+    {
+        title: "Royal double Burger",
+        dicription: "Try our best royal double burger",
+        price: 500,
+        img: double,
+        alt: "Royal double Burger"
+
+    },
+    {
+        title: "Beef Burger" ,
+        dicription: "Try our best Beef Burger",
+        price: 200,
+        img: beef,
+        alt: "Beef Burger" 
+
+    },
+
+    {
+        title: "Royal double Burger",
+        dicription: "Try our best royal double burger",
+        price: 500,
+        img: double,
+        alt: "Royal double Burger"
+
+
+    },
+
+
+];
+
+
+

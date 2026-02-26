e8b290dab8616cf13c7c1c9d1bc90a79a1b1ca52
Segni Kasahun <segnikasahun969@gmail.com>
2026-02-26 22:45:47 +0300

    Add story section to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,14 @@ const About = () => {
                 <div className="flex-1 flex items-center justify-center">
                     <img src={photo} width={400} height={400} className=" rounded-lg " alt="about" />
                 </div>
+              <div className="flex-1 pr-20">
+                    <img src="" alt="" />
+                    <p className='text-green-900 font-normal font-poppins'>Our Story
+                        At the heart of the Royal Burger, Café & Restaurant is a dream — a dream to craft a haven where nature's beauty meets the finest culinary delights. Our journey began with a simple idea: to create a space where people could escape the daily grind and immerse themselves in a serene garden setting while savoring dishes that are as delightful as the surroundings.<br/>
+
+                        From humble beginnings, our founders sought inspiration from gardens worldwide, blending their love for natural landscapes with a passion for gourmet cuisine. Every corner of our café reflects this dedication, with blooming flowers, lush greenery, and tranquil pathways creating an enchanting atmosphere.<br/>
+                    </p>
+                </div>
                 
             </div>
         </div>

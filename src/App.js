5577a60deddb863bcab6a87e382f2e1018799880
Segni Kasahun <segnikasahun969@gmail.com>
2026-04-02 22:06:05 +0300

    Add imports for new components in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import Hero from "./components/Hero";
 import Product from "./components/Product";
 import Service from "./components/Service";
 
